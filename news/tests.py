7c14ac16712c8db4027353edb711e4ac0ea209cd
omarion3698 <omaribinbakarivic@gmail.com>
2020-10-14 00:44:42 +0300

    Fix images failure

--- news/tests.py
+++ news/tests.py
@@ -14,15 +14,19 @@ class EditorTestClass(TestCase):
         self.assertTrue(isinstance(self.omar,Editor))
 
 class ArticleTestClass(TestCase):
+    
     def setUp(self):
         # Creating a new editor and saving it
-        self.omar= Editor(first_name = 'Omar', last_name ='Hussein', email ='omaribinbakarivic@gmail.com')
-        self.omar.save_editor()
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.james.save_editor()
+        
         # Creating a new tag and saving it
         self.new_tag = tags(name = 'testing')
         self.new_tag.save()
-        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.omar)
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
         self.new_article.save()
+
         self.new_article.tags.add(self.new_tag)
 
     def tearDown(self):
--- news/views.py
+++ news/views.py
@@ -4,10 +4,7 @@ import datetime as dt
 from .models import Article
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
-
-def news_of_day(request):
+def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
     return render(request, 'all-news/today-news.html', {"date": date,"news":news})
@@ -21,8 +18,10 @@ def past_days_news(request, past_date):
         # Raise 404 error when ValueError is thrown
         raise Http404()
         assert False
+
     if date == dt.date.today():
         return redirect(news_today)
+        
     news = Article.days_news(date)
     return render(request, 'all-news/past-news.html',{"date": date,"news":news})
 
